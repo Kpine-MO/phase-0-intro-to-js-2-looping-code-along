bad484c24f246206c30bb4f296f225c765e9b450
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-03 10:11:14 +0300

    modification of index.js file

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+	let messages = names.map(
+		(name) => `Thank you, ${name}, for the wonderful surprise gift!`
+	);
+    return messages;
+}
+
+function countDown(num) {
+    num = 0;
+    for(let i=0; i<11; i++) {
+        num++
+        console.log(i);
+    }
+};
